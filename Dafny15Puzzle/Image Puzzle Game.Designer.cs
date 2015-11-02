b069108492ada39743bfa746f60735969b5e0ad1
DerBofrostMann <marcelselle.MS@gmail.com>
2015-11-02 15:08:01 +0100

    Swap

---
+++ Dafny15Puzzle/Image Puzzle Game.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Dafny15Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseImageBox = new System.Windows.Forms.GroupBox();
+            this.ChooseImageButton = new System.Windows.Forms.Button();
+            this.textboxImagePath = new System.Windows.Forms.TextBox();
+            this.PuzzleBox = new System.Windows.Forms.GroupBox();
+            this.StatusBox = new System.Windows.Forms.GroupBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SteuerungsBox = new System.Windows.Forms.GroupBox();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ChooseImageBox.SuspendLayout();
+            this.StatusBox.SuspendLayout();
+            this.SteuerungsBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChooseImageBox
+            // 
+            this.ChooseImageBox.Controls.Add(this.ChooseImageButton);
+            this.ChooseImageBox.Controls.Add(this.textboxImagePath);
+            this.ChooseImageBox.Location = new System.Drawing.Point(12, 12);
+            this.ChooseImageBox.Name = "ChooseImageBox";
+            this.ChooseImageBox.Size = new System.Drawing.Size(794, 100);
+            this.ChooseImageBox.TabIndex = 0;
+            this.ChooseImageBox.TabStop = false;
+            this.ChooseImageBox.Text = "Choose Image";
+            // 
+            // ChooseImageButton
+            // 
+            this.ChooseImageButton.Location = new System.Drawing.Point(682, 44);
+            this.ChooseImageButton.Name = "ChooseImageButton";
+            this.ChooseImageButton.Size = new System.Drawing.Size(106, 23);
+            this.ChooseImageButton.TabIndex = 1;
+            this.ChooseImageButton.Text = "...";
+            this.ChooseImageButton.UseVisualStyleBackColor = true;
+            this.ChooseImageButton.Click += new System.EventHandler(this.buttonImageBrowse_Click);
+            // 
+            // textboxImagePath
+            // 
+            this.textboxImagePath.Location = new System.Drawing.Point(6, 47);
+            this.textboxImagePath.Name = "textboxImagePath";
+            this.textboxImagePath.Size = new System.Drawing.Size(647, 20);
+            this.textboxImagePath.TabIndex = 0;
+            // 
+            // PuzzleBox
+            // 
+            this.PuzzleBox.Location = new System.Drawing.Point(13, 136);
+            this.PuzzleBox.Name = "PuzzleBox";
+            this.PuzzleBox.Size = new System.Drawing.Size(444, 444);
+            this.PuzzleBox.TabIndex = 1;
+            this.PuzzleBox.TabStop = false;
+            this.PuzzleBox.Text = "Puzzle";
+            this.PuzzleBox.Click += new System.EventHandler(this.picBoxes_click);
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.Controls.Add(this.groupBox2);
+            this.StatusBox.Controls.Add(this.groupBox1);
+            this.StatusBox.Controls.Add(this.labelStatus);
+            this.StatusBox.Location = new System.Drawing.Point(527, 150);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(279, 122);
+            this.StatusBox.TabIndex = 2;
+            this.StatusBox.TabStop = false;
+            this.StatusBox.Text = "Status";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(114, 96);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(24, 13);
+            this.labelStatus.TabIndex = 0;
+            this.labelStatus.Text = "Idle";
+            // 
+            // SteuerungsBox
+            // 
+            this.SteuerungsBox.Controls.Add(this.RestartButton);
+            this.SteuerungsBox.Location = new System.Drawing.Point(527, 446);
+            this.SteuerungsBox.Name = "SteuerungsBox";
+            this.SteuerungsBox.Size = new System.Drawing.Size(279, 107);
+            this.SteuerungsBox.TabIndex = 3;
+            this.SteuerungsBox.TabStop = false;
+            this.SteuerungsBox.Text = "Steuerung";
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(6, 19);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(105, 27);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(80, 40);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Spiel-Zeit";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(180, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(76, 55);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 594);
+            this.Controls.Add(this.SteuerungsBox);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.PuzzleBox);
+            this.Controls.Add(this.ChooseImageBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "15er Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ChooseImageBox.ResumeLayout(false);
+            this.ChooseImageBox.PerformLayout();
+            this.StatusBox.ResumeLayout(false);
+            this.StatusBox.PerformLayout();
+            this.SteuerungsBox.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox ChooseImageBox;
+        private System.Windows.Forms.Button ChooseImageButton;
+        private System.Windows.Forms.TextBox textboxImagePath;
+        private System.Windows.Forms.GroupBox PuzzleBox;
+        private System.Windows.Forms.GroupBox StatusBox;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.GroupBox SteuerungsBox;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Dafny15Puzzle/Image Puzzle Game.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dafny15Puzzle
+{
+    public partial class Form1 : Form
+    {
+        private readonly int[] _bordersNums = { 0, 4, 8, 12, 3, 7, 11, 15 };
+        private int MoveableBox;
+        OpenFileDialog openFileDialog = null;
+        Image image;
+        PictureBox picBoxWhole = null;
+        PictureBox[] picBoxes = null;
+        Image[] images = null;
+        const int BOX_NUM = 16;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void NewGame()
+        {   
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            
+        }
+        /*
+         * Vertauscht das Bild einer PicBox mit dem Bild der MoveAble PicBox 
+         */
+        void SwapBoxes(int BoxNumber)
+        {   Image dummy = picBoxes[BoxNumber].Image;
+
+            picBoxes[BoxNumber].Image = picBoxes[MoveableBox].Image;
+            picBoxes[MoveableBox].Image = dummy;
+            MoveableBox = BoxNumber;
+
+        }
+
+        void picBoxes_click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+        /*
+         * Click-Event des ChooseImageButtons
+         */
+        private void buttonImageBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog == null)
+                openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                image = CreateBitmapImage(Image.FromFile(openFileDialog.FileName));
+
+                if (picBoxWhole == null)
+                {
+                    picBoxWhole = new PictureBox();
+                    picBoxWhole.Height  =   PuzzleBox.Height;
+                    picBoxWhole.Width   =   PuzzleBox.Width;
+                    PuzzleBox.Controls.Add(picBoxWhole);
+                }
+
+
+                splitImagesToPicBoxes();
+                MoveableBox = 15;
+                clearBox(picBoxes[15]);
+    
+            }
+        }
+
+        /*
+         * Löscht das Image einer PicBox
+         */
+        private void clearBox(PictureBox pictureBox)
+        {
+            Graphics objGraphics = Graphics.FromImage(pictureBox.Image);
+            objGraphics.Clear(Color.White); 
+        }
+
+   
+
+        /*
+         * Zerteilt das Bild in 16 PictureBoxen mit jeweils 16 Images
+         */
+        private void splitImagesToPicBoxes()
+        {
+            if (picBoxWhole != null)
+            {
+                PuzzleBox.Controls.Remove(picBoxWhole);
+                picBoxWhole.Dispose();
+                picBoxWhole = null;
+            }
+            if (picBoxes == null)
+            {
+                images = new Image[BOX_NUM];
+                picBoxes = new PictureBox[BOX_NUM];
+            }
+            int numRow = 4;
+            int numCol = 4;
+            int unitX = PuzzleBox.Width / numRow;
+            int unitY = PuzzleBox.Height / numCol;
+            int[] indice = new int[BOX_NUM];
+            for (int i = 0; i < BOX_NUM; i++)
+            {
+                indice[i] = i;
+                if (picBoxes[i] == null)
+                {
+                    picBoxes[i] = new PictureBox();
+                    picBoxes[i].BorderStyle = BorderStyle.Fixed3D;
+                }
+                    
+                picBoxes[i].Width = unitX;
+                picBoxes[i].Height = unitY;
+
+
+                picBoxes[i].Image = CreateBitmapImage(image,i, numRow, numCol, unitX, unitY);
+                picBoxes[i].Location = new Point(unitX * (i % numCol), unitY * (i / numCol));
+                PuzzleBox.Controls.Add(picBoxes[i]);
+            }
+
+        }
+        /*
+         * Erzeugt eine skalierte Bitmap für die PicBoxWhole aus einem Image 
+         */
+        private Bitmap CreateBitmapImage(Image image)
+        {
+            Bitmap objBmpImage = new Bitmap(PuzzleBox.Width, PuzzleBox.Height);
+            Graphics objGraphics = Graphics.FromImage(objBmpImage);
+            objGraphics.Clear(Color.White);
+            objGraphics.DrawImage(image, new Rectangle(0, 0, PuzzleBox.Width, PuzzleBox.Height));
+            objGraphics.Flush();
+
+            return objBmpImage;
+        }
+        /*
+         * Erzeugt eine Skalierte Bitmap für die jeweiligen kleinen PicBoxen
+         */
+        private Bitmap CreateBitmapImage(Image image, int index, int numRow, int numCol, int unitX, int unitY)
+        {
+            Bitmap objBmpImage = new Bitmap(unitX, unitY);
+            Graphics objGraphics = Graphics.FromImage(objBmpImage);
+            objGraphics.Clear(Color.White);
+
+            objGraphics.DrawImage(image, new Rectangle(0, 0, unitX, unitY), new Rectangle(unitX * (index % numCol), unitY * (index / numCol), unitX, unitY), GraphicsUnit.Pixel);
+            objGraphics.Flush();
+
+            return objBmpImage;
+        }
+
+        /*
+         * Click-Event des RestartButtons
+         */
+        private void restart_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            SwapBoxes(rand.Next(15));
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+
+
+    }
+}
